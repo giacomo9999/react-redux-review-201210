46d5176956eb6c844bf6697f2c74b366a27e5d85
giacomo9999 <gmarchesi888@gmail.com>
2020-12-11 16:11:57 -0500

    Rusty AF after a couple months illustrating.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>I AM THE APP</h1>
-    </div>
-  );
+class App extends Component {
+  state = { currentNameIndex: 0, personNames: ["Alex", "Jim", "Carol"] };
+
+  switchNameHandler = () => {
+    console.log("Switching name...", this.state.currentNameIndex + 1);
+    this.setState({ currentNameIndex: this.state.currentNameIndex + 1 });
+    console.log(this.state);
+  };
+
+  render() {
+    const nameToDisplay = (
+      <Person name={this.state.personNames[this.state.currentNameIndex]} />
+    );
+
+    return (
+      <div className="App">
+        <h1>I AM THE APP</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        {nameToDisplay}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const person = (props) => {
+  return <p>I Am A Person Named {props.name}</p>;
+};
+
+export default person;
