b57d27dc1533913120d1599901da3f6f82512548
giacomo9999 <gmarchesi888@gmail.com>
2020-12-11 11:19:39 -0500

    Deleted boilerplate, wrote new test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appText = screen.getByText(/App/i);
+  expect(appText).toBeInTheDocument();
 });
