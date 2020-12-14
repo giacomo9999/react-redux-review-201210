bfae685168d118b95ff4d6e6459ba6d97f4ae840
giacomo9999 <gmarchesi888@gmail.com>
2020-12-14 17:48:37 -0500

    Switched back from Hooks to traditional state.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,52 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-const App = (props) => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
+    showPersonsList: true,
     currentNameIndex: 0,
     personsData: [
       { name: "Alex", age: 38 },
       { name: "Jim", age: 53 },
       { name: "Carol", age: 50 },
+      { name: "END OF FILE", age: -999 },
     ],
-  });
-
-  const switchPersonHandler = () => {
-    console.log("Switching name...");
-    setPersonsState({
-      currentNameIndex: personsState.currentNameIndex + 1,
-      personsData: personsState.personsData,
-    });
-    console.log(personsState);
   };
 
-  return (
-    <div className="App">
-      <h1>I AM THE APP</h1>
-      <button onClick={switchPersonHandler}>Switch Name</button>
-      {
-        <Person
-          name={personsState.personsData[personsState.currentNameIndex].name}
-          age={personsState.personsData[personsState.currentNameIndex].age}
-        />
-      }
-    </div>
-  );
-};
+  switchPersonHandler = () => {
+    if (this.state.currentNameIndex + 1 > 2) {
+      console.log("End of data...");
+      this.setState({
+        currentNameIndex: 3,
+        personsData: this.state.personsData,
+      });
+    } else {
+      console.log("Switching name...");
+      this.setState({
+        currentNameIndex: this.state.currentNameIndex + 1,
+        personsData: this.state.personsData,
+      });
+      console.log(this.state);
+    }
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>I AM THE APP</h1>
+        <button onClick={this.switchPersonHandler}>Switch Name</button>
+        {this.state.showPersonsList ? (
+          <Person
+            name={this.state.personsData[this.state.currentNameIndex].name}
+            age={this.state.personsData[this.state.currentNameIndex].age}
+            clickAction={this.switchPersonHandler}
+          />
+        ) : (
+          <h1>--</h1>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const person = (props) => {
   return (
-    <p>
-      I Am A Person Named {props.name} and my age is {props.age}
-    </p>
+    <div>
+      <p>
+        I Am A Person Named {props.name} and my age is {props.age}
+      </p>
+      <button onClick={props.clickAction}>Click to Increment Person</button>
+    </div>
   );
 };
 
